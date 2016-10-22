1327761823d22ffe627e814aeb93642ea75868ce
尼古拉斯 赵四 <742880412@qq.com>
2016-10-22 10:33:50 +0800

    完成了设计师界面的tablayout框架

--- app/src/main/java/com/lanou3g/mostbeautifulproperty/designer/DesignerFragment.java
+++ app/src/main/java/com/lanou3g/mostbeautifulproperty/designer/DesignerFragment.java
@@ -1,13 +1,26 @@
 package com.lanou3g.mostbeautifulproperty.designer;
 
+import android.graphics.Color;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+
 import com.lanou3g.mostbeautifulproperty.R;
 import com.lanou3g.mostbeautifulproperty.baseclass.BaseFragment;
+import com.lanou3g.mostbeautifulproperty.discover.DiscoverAdapter;
+import com.lanou3g.mostbeautifulproperty.discover.tabreuse.DiscoverTabReuseFragment;
+
+import java.util.ArrayList;
 
 /**
  *
  */
 
 public class DesignerFragment extends BaseFragment{
+
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+
     @Override
     protected int setLayout() {
         return R.layout.fragment_designer;
@@ -15,11 +28,38 @@ public class DesignerFragment extends BaseFragment{
 
     @Override
     protected void initView() {
-
+        mViewPager = bindView(R.id.vp_designer);
+        mTabLayout = bindView(R.id.tab_designer);
     }
 
     @Override
     protected void initData() {
+        ArrayList<String> titles = initTitles();
 
+        ArrayList<Fragment> fragments = new ArrayList<>();
+        for (int i = 0; i < titles.size(); i++) {
+            fragments.add(new DiscoverTabReuseFragment());
+        }
+
+        DesignerAdapter adapter = new DesignerAdapter(getChildFragmentManager());
+        adapter.setTitles(titles);
+        adapter.setFragments(fragments);
+
+        mViewPager.setAdapter(adapter);
+        mTabLayout.setupWithViewPager(mViewPager);
+        // 更改tab 下滑线
+        mTabLayout.setSelectedTabIndicatorColor(Color.WHITE);
+        // 给tab文字 加选中颜色
+        mTabLayout.setTabTextColors(Color.GRAY, Color.WHITE);
+    }
+
+    private ArrayList<String> initTitles() {
+        ArrayList<String>titles = new ArrayList<>();
+        titles.add("我关注的");
+        titles.add("推荐");
+        titles.add("最受欢迎");
+        titles.add("独立设计师");
+        titles.add("大牌设计师");
+        return titles;
     }
 }
---
+++ app/src/main/java/com/lanou3g/mostbeautifulproperty/designer/tabreuse/DesignerTabReuseFragment.java
@@ -0,0 +1,41 @@
+package com.lanou3g.mostbeautifulproperty.designer.tabreuse;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.lanou3g.mostbeautifulproperty.R;
+import com.lanou3g.mostbeautifulproperty.baseclass.BaseFragment;
+
+/**
+ * Created by dllo on 16/10/22.
+ */
+
+public class DesignerTabReuseFragment extends BaseFragment {
+    TextView mTextView;
+
+    public static DesignerTabReuseFragment newInstance(int position) {
+
+        Bundle args = new Bundle();
+        args.putInt("i",position);
+        DesignerTabReuseFragment fragment = new DesignerTabReuseFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    protected int setLayout() {
+        return R.layout.fragment_designer_reuse;
+    }
+
+    @Override
+    protected void initView() {
+        mTextView = bindView(R.id.tv_designer_reuse);
+    }
+
+    @Override
+    protected void initData() {
+        Bundle args = getArguments();
+        int position = args.getInt("i");
+        mTextView.setText(position + "");
+    }
+}
