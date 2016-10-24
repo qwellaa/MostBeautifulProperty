f0f006a43556169b48559890ba8ce0afb0e5b252
张作发 <291390214@qq.com>
2016-10-24 21:54:11 +0800

    登录界面

--- app/src/main/java/com/lanou3g/mostbeautifulproperty/mine/uiview/MineFragment.java
+++ app/src/main/java/com/lanou3g/mostbeautifulproperty/mine/uiview/MineFragment.java
@@ -1,13 +1,21 @@
 package com.lanou3g.mostbeautifulproperty.mine.uiview;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.LinearLayout;
+
 import com.lanou3g.mostbeautifulproperty.R;
 import com.lanou3g.mostbeautifulproperty.baseclass.BaseFragment;
+import com.lanou3g.mostbeautifulproperty.mine.uiview.chat.LoginActivity;
 
 /**
  *
  */
 
-public class MineFragment extends BaseFragment{
+public class MineFragment extends BaseFragment implements View.OnClickListener {
+
+    private LinearLayout mMessageLl;
+
     @Override
     protected int setLayout() {
         return R.layout.fragment_mine;
@@ -15,11 +23,25 @@ public class MineFragment extends BaseFragment{
 
     @Override
     protected void initView() {
+        // 消息中心的点击事件
+        mMessageLl = bindView(R.id.fragment_mine_message_ll);
+        mMessageLl.setOnClickListener(this);
 
     }
 
     @Override
     protected void initData() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.fragment_mine_message_ll:
+                Intent loginIntent = new Intent(getContext(),LoginActivity.class);
+                startActivity(loginIntent);
+                break;
+        }
 
     }
 }
---
+++ app/src/main/java/com/lanou3g/mostbeautifulproperty/mine/uiview/chat/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.lanou3g.mostbeautifulproperty.mine.uiview.chat;
+
+import com.lanou3g.mostbeautifulproperty.R;
+import com.lanou3g.mostbeautifulproperty.baseclass.BaseActivity;
+
+/**
+ * Created by dllo on 16/10/24.
+ */
+
+public class LoginActivity extends BaseActivity {
+    @Override
+    protected int setLayout() {
+        return R.layout.login_activity;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
