44886f727e612bd8aaffbd32c7d30dbb059d19b1
尼古拉斯 赵四 <742880412@qq.com>
2016-10-24 15:50:24 +0800

    修改布局控件

--- app/src/main/java/com/lanou3g/mostbeautifulproperty/designer/DesignerFragment.java
+++ app/src/main/java/com/lanou3g/mostbeautifulproperty/designer/DesignerFragment.java
@@ -1,14 +1,13 @@
 package com.lanou3g.mostbeautifulproperty.designer;
 
-import android.graphics.Color;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 
 import com.lanou3g.mostbeautifulproperty.R;
 import com.lanou3g.mostbeautifulproperty.baseclass.BaseFragment;
-import com.lanou3g.mostbeautifulproperty.discover.DiscoverAdapter;
 import com.lanou3g.mostbeautifulproperty.discover.tabreuse.DiscoverTabReuseFragment;
+
+import net.lucode.hackware.magicindicator.MagicIndicator;
 
 import java.util.ArrayList;
 
@@ -18,8 +17,9 @@ import java.util.ArrayList;
 
 public class DesignerFragment extends BaseFragment{
 
-    private TabLayout mTabLayout;
+    private MagicIndicator mTabLayout;
     private ViewPager mViewPager;
+    private ArrayList<String> mTitles;
 
     @Override
     protected int setLayout() {
@@ -46,20 +46,24 @@ public class DesignerFragment extends BaseFragment{
         adapter.setFragments(fragments);
 
         mViewPager.setAdapter(adapter);
-        mTabLayout.setupWithViewPager(mViewPager);
-        // 更改tab 下滑线
-        mTabLayout.setSelectedTabIndicatorColor(Color.WHITE);
-        // 给tab文字 加选中颜色
-        mTabLayout.setTabTextColors(Color.GRAY, Color.WHITE);
+        initMagic();
+//        mTabLayout.setupWithViewPager(mViewPager);
+//        // 更改tab 下滑线
+//        mTabLayout.setSelectedTabIndicatorColor(Color.WHITE);
+//        // 给tab文字 加选中颜色
+//        mTabLayout.setTabTextColors(Color.GRAY, Color.WHITE);
+    }
+
+    private void initMagic() {
     }
 
     private ArrayList<String> initTitles() {
-        ArrayList<String>titles = new ArrayList<>();
-        titles.add("我关注的");
-        titles.add("推荐");
-        titles.add("最受欢迎");
-        titles.add("独立设计师");
-        titles.add("大牌设计师");
-        return titles;
+        mTitles = new ArrayList<>();
+        mTitles.add("我关注的");
+        mTitles.add("推荐");
+        mTitles.add("最受欢迎");
+        mTitles.add("独立设计师");
+        mTitles.add("大牌设计师");
+        return mTitles;
     }
 }
