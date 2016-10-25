92e6a2628c862e4afd4854c23418a6dc59436117
尼古拉斯 赵四 <742880412@qq.com>
2016-10-25 19:47:15 +0800

    添加测试数据

--- app/src/main/java/com/lanou3g/mostbeautifulproperty/bean/DesignerBean.java
+++ app/src/main/java/com/lanou3g/mostbeautifulproperty/bean/DesignerBean.java
@@ -7,4 +7,20 @@ package com.lanou3g.mostbeautifulproperty.bean;
 public class DesignerBean {
     String tv1;
     String tv2;
+
+    public String getTv1() {
+        return tv1;
+    }
+
+    public void setTv1(String tv1) {
+        this.tv1 = tv1;
+    }
+
+    public String getTv2() {
+        return tv2;
+    }
+
+    public void setTv2(String tv2) {
+        this.tv2 = tv2;
+    }
 }
