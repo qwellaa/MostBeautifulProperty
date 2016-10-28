093eb927a2a2e65ca5688af0729a08feab7ce100
张作发 <291390214@qq.com>
2016-10-28 20:48:26 +0800

    比较丑的的注册和登录

--- app/src/main/java/com/lanou3g/mostbeautifulproperty/mine/uiview/chat/LoginActivity.java
+++ app/src/main/java/com/lanou3g/mostbeautifulproperty/mine/uiview/chat/LoginActivity.java
@@ -1,5 +1,18 @@
 package com.lanou3g.mostbeautifulproperty.mine.uiview.chat;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.hyphenate.EMCallBack;
+import com.hyphenate.EMError;
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.exceptions.HyphenateException;
 import com.lanou3g.mostbeautifulproperty.R;
 import com.lanou3g.mostbeautifulproperty.baseclass.BaseActivity;
 
@@ -7,20 +20,226 @@ import com.lanou3g.mostbeautifulproperty.baseclass.BaseActivity;
  * Created by dllo on 16/10/24.
  */
 
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends BaseActivity implements View.OnClickListener {
+    private ProgressDialog mDialog;
+    private EditText mUserNameEdit;
+    private EditText mPasswordEdit;
+    private Button mRegisterBtn;
+    private Button mLoginBtn;
+
     @Override
     protected int setLayout() {
         return R.layout.login_activity;
+
+
     }
 
 
     @Override
     protected void initView() {
+        mUserNameEdit = bindView(R.id.login_username);
+        mPasswordEdit = bindView(R.id.login_password);
+        mRegisterBtn = bindView(R.id.button_riegister);
+        mLoginBtn = bindView(R.id.btn_login);
+        mRegisterBtn.setOnClickListener(this);
+        mLoginBtn.setOnClickListener(this);
+
 
     }
 
     @Override
     protected void initData() {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_riegister:
+                initSignUp();
+                break;
+            case R.id.btn_login:
+                initLoginIn();
+
+                break;
+        }
+
+    }
+
+    private void initSignUp() {
+        mDialog = new ProgressDialog(this);
+        mDialog.setMessage("大爷, 注册请稍等..");
+        mDialog.show();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String userName = mUserNameEdit.getText().toString().trim();
+                    String passWord = mPasswordEdit.getText().toString().trim();
+                    EMClient.getInstance().createAccount(userName, passWord);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (!LoginActivity.this.isFinishing()) {
+                                mDialog.dismiss();
+                            }
+                            Toast.makeText(LoginActivity.this, "注册成功", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } catch (final HyphenateException e) {
+                    e.printStackTrace();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (!LoginActivity.this.isFinishing()) {
+                                mDialog.dismiss();
+                            }
+                            int errorCode = e.getErrorCode();
+                            String message = e.getMessage();
+                            switch (errorCode) {
+
+                                // 网络错误
+                                case EMError.NETWORK_ERROR:
+                                    Toast.makeText(LoginActivity.this, "网络错误 code: " + errorCode + ", message:" + message, Toast.LENGTH_LONG).show();
+                                    break;
+                                // 用户已存在
+                                case EMError.USER_ALREADY_EXIST:
+                                    Toast.makeText(LoginActivity.this, "用户已存在 code: " + errorCode + ", message:" + message, Toast.LENGTH_LONG).show();
+                                    break;
+                                // 参数不合法，一般情况是username 使用了uuid导致，不能使用uuid注册
+                                case EMError.USER_ILLEGAL_ARGUMENT:
+                                    Toast.makeText(LoginActivity.this, "参数不合法，一般情况是username 使用了uuid导致，不能使用uuid注册 code: " + errorCode + ", message:" + message, Toast.LENGTH_LONG).show();
+                                    break;
+                                // 服务器未知错误
+                                case EMError.SERVER_UNKNOWN_ERROR:
+                                    Toast.makeText(LoginActivity.this, "服务器未知错误 code: " + errorCode + ", message:" + message, Toast.LENGTH_LONG).show();
+                                    break;
+                                case EMError.USER_REG_FAILED:
+                                    Toast.makeText(LoginActivity.this, "账户注册失败 code: " + errorCode + ", message:" + message, Toast.LENGTH_LONG).show();
+                                    break;
+                                default:
+                                    Toast.makeText(LoginActivity.this, "ml_sign_up_failed code: " + errorCode + ", message:" + message, Toast.LENGTH_LONG).show();
+                                    break;
+
+
+                            }
+
+
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }).start();
+    }
+
+
+    /**
+     * 登录方法
+     */
+    private void initLoginIn() {
+        mDialog = new ProgressDialog(this);
+        mDialog.setMessage("正在登陆，请稍后...");
+        mDialog.show();
+        String username = mUserNameEdit.getText().toString().trim();
+        String password = mPasswordEdit.getText().toString().trim();
+        if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
+            Toast.makeText(LoginActivity.this, "用户名和密码不能为空", Toast.LENGTH_LONG).show();
+            return;
+        }
+        EMClient.getInstance().login(username, password, new EMCallBack() {
+            /**
+             * 登陆成功的回调
+             */
+            @Override
+            public void onSuccess() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mDialog.dismiss();
+
+                        // 加载所有会话到内存
+                        EMClient.getInstance().chatManager().loadAllConversations();
+                        // 加载所有群组到内存，如果使用了群组的话
+                        // EMClient.getInstance().groupManager().loadAllGroups();
+
+                        // 登录成功跳转界面
+                        Intent intent = new Intent(LoginActivity.this, MessageMainActivity.class);
+                        Log.d("ECLoginActivity", "哈哈");
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+            }
+
+            /**
+             * 登陆错误的回调
+             * @param i
+             * @param s
+             */
+            @Override
+            public void onError(final int i, final String s) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mDialog.dismiss();
+                        Log.d("lzan13", "登录失败 Error code:" + i + ", message:" + s);
+                        /**
+                         * 关于错误码可以参考官方api详细说明
+                         * http://www.easemob.com/apidoc/android/chat3.0/classcom_1_1hyphenate_1_1_e_m_error.html
+                         */
+                        switch (i) {
+                            // 网络异常 2
+                            case EMError.NETWORK_ERROR:
+                                Toast.makeText(LoginActivity.this, "网络错误 code: " + i + ", message:" + s, Toast.LENGTH_LONG).show();
+                                break;
+                            // 无效的用户名 101
+                            case EMError.INVALID_USER_NAME:
+                                Toast.makeText(LoginActivity.this, "无效的用户名 code: " + i + ", message:" + s, Toast.LENGTH_LONG).show();
+                                break;
+                            // 无效的密码 102
+                            case EMError.INVALID_PASSWORD:
+                                Toast.makeText(LoginActivity.this, "无效的密码 code: " + i + ", message:" + s, Toast.LENGTH_LONG).show();
+                                break;
+                            // 用户认证失败，用户名或密码错误 202
+                            case EMError.USER_AUTHENTICATION_FAILED:
+                                Toast.makeText(LoginActivity.this, "用户认证失败，用户名或密码错误 code: " + i + ", message:" + s, Toast.LENGTH_LONG).show();
+                                break;
+                            // 用户不存在 204
+                            case EMError.USER_NOT_FOUND:
+                                Toast.makeText(LoginActivity.this, "用户不存在 code: " + i + ", message:" + s, Toast.LENGTH_LONG).show();
+                                break;
+                            // 无法访问到服务器 300
+                            case EMError.SERVER_NOT_REACHABLE:
+                                Toast.makeText(LoginActivity.this, "无法访问到服务器 code: " + i + ", message:" + s, Toast.LENGTH_LONG).show();
+                                break;
+                            // 等待服务器响应超时 301
+                            case EMError.SERVER_TIMEOUT:
+                                Toast.makeText(LoginActivity.this, "等待服务器响应超时 code: " + i + ", message:" + s, Toast.LENGTH_LONG).show();
+                                break;
+                            // 服务器繁忙 302
+                            case EMError.SERVER_BUSY:
+                                Toast.makeText(LoginActivity.this, "服务器繁忙 code: " + i + ", message:" + s, Toast.LENGTH_LONG).show();
+                                break;
+                            // 未知 Server 异常 303 一般断网会出现这个错误
+                            case EMError.SERVER_UNKNOWN_ERROR:
+                                Toast.makeText(LoginActivity.this, "未知的服务器异常 code: " + i + ", message:" + s, Toast.LENGTH_LONG).show();
+                                break;
+                            default:
+                                Toast.makeText(LoginActivity.this, "ml_sign_in_failed code: " + i + ", message:" + s, Toast.LENGTH_LONG).show();
+                                break;
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onProgress(int i, String s) {
+
+            }
+        });
     }
 }
