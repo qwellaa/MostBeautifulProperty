932af511e1b816e514425845a03e9117f989ecd0
张作发 <291390214@qq.com>
2016-11-08 20:51:41 +0800

    fragment替换完成

--- app/src/main/java/com/lanou3g/mostbeautifulproperty/homepage/MainActivity.java
+++ app/src/main/java/com/lanou3g/mostbeautifulproperty/homepage/MainActivity.java
@@ -21,6 +21,16 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     private RadioButton mRBtnDesigner;
     private RadioButton mRBtnDiscover;
     private RadioButton mRBtnMine;
+    private static final int FRAGMENT_MAGAZINE = 0;
+    private static final int FRAGMENT_DISCOVER = 1;
+    private static final int FRAGMENT_DESIGNER = 2;
+    private static final int FRAGMENT_MINE = 3;
+    private FragmentManager mManager;
+    private MagazineFragment mMagazineFragment;
+    private DiscoverFragment mDiscoverFragment;
+    private DesignerFragment mDesignerFragment;
+    private MineFragment mMineFragment;
+
 
     @Override
     protected int setLayout() {
@@ -49,19 +59,19 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     }
 
 
-
     private void initStartRun() {
         mRBtnMagazine.setChecked(true);
 
         // 默认显示的fragment
-        FragmentManager manager = getSupportFragmentManager();
+        mManager = getSupportFragmentManager();
+        showFragment(FRAGMENT_MAGAZINE);
         /**
          * 使用Fragment时，可以通过用户交互来执行一些动作，比如增加、移除、替换等。
          * 所有这些改变构成一个集合，这个集合被叫做一个transaction。
          */
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.replace(R.id.replace_view, new MagazineFragment());
-        transaction.commit();
+//        FragmentTransaction transaction = mManager.beginTransaction();
+//        transaction.replace(R.id.replace_view, new MagazineFragment());
+//        transaction.commit();
     }
 
     @Override
@@ -72,20 +82,91 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
         switch (v.getId()) {
             case R.id.radio_btn_main_magazine:
-                transaction.replace(R.id.replace_view, new MagazineFragment());
+              //  transaction.replace(R.id.replace_view, new MagazineFragment());
+                showFragment(FRAGMENT_MAGAZINE);
                 break;
             case R.id.radio_btn_main_discover:
-                transaction.replace(R.id.replace_view, new DiscoverFragment());
+         //       transaction.replace(R.id.replace_view, new DiscoverFragment());
+                showFragment(FRAGMENT_DISCOVER);
                 break;
             case R.id.radio_btn_main_designer:
-                transaction.replace(R.id.replace_view, new DesignerFragment());
+         //       transaction.replace(R.id.replace_view, new DesignerFragment());
+                showFragment(FRAGMENT_DESIGNER);
                 break;
             case R.id.radio_btn_main_mine:
-                transaction.replace(R.id.replace_view, new MineFragment());
+         //       transaction.replace(R.id.replace_view, new MineFragment());
+                showFragment(FRAGMENT_MINE);
                 break;
         }
         transaction.commit();
     }
+
+
+    private void showFragment(int index) {
+        FragmentTransaction transaction = mManager.beginTransaction();
+        hideFragment(transaction);
+        switch (index) {
+
+            case FRAGMENT_MAGAZINE:
+                if (mMagazineFragment == null) {
+                    mMagazineFragment = new MagazineFragment();
+                    transaction.add(R.id.replace_view, mMagazineFragment);
+                } else {
+                    transaction.show(mMagazineFragment);
+
+                }
+                break;
+            case FRAGMENT_DISCOVER:
+                if (mDiscoverFragment == null) {
+                    mDiscoverFragment = new DiscoverFragment();
+                    transaction.add(R.id.replace_view, mDiscoverFragment);
+                } else {
+                    transaction.show(mDiscoverFragment);
+
+                }
+                break;
+            case FRAGMENT_DESIGNER:
+                if (mDesignerFragment == null) {
+                    mDesignerFragment = new DesignerFragment();
+                    transaction.add(R.id.replace_view, mDesignerFragment);
+                } else {
+                    transaction.show(mDesignerFragment);
+
+                }
+                break;
+            case FRAGMENT_MINE:
+                if (mMineFragment == null) {
+                    mMineFragment = new MineFragment();
+                    transaction.add(R.id.replace_view, mMineFragment);
+                } else {
+                    transaction.show(mMineFragment);
+
+                }
+
+                break;
+        }
+        transaction.commit();
+
+
+    }
+
+    private void hideFragment(FragmentTransaction transaction) {
+        if (mMagazineFragment !=null){
+            transaction.hide(mMagazineFragment);
+        }
+        if (mDiscoverFragment !=null){
+            transaction.hide(mDiscoverFragment);
+        }
+        if (mDesignerFragment !=null){
+            transaction.hide(mDesignerFragment);
+        }
+        if (mMineFragment !=null){
+            transaction.hide(mMineFragment);
+        }
+
+
+    }
+
     public interface MyTouchListener {
         public void onTouchEvent(MotionEvent event);
     }
@@ -95,6 +176,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     /**
      * 提供给Fragment通过getActivity()方法来注册自己的触摸事件的方法
+     *
      * @param listener
      */
     public void registerMyTouchListener(MyTouchListener listener) {
@@ -103,10 +185,11 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     /**
      * 提供给Fragment通过getActivity()方法来取消注册自己的触摸事件的方法
+     *
      * @param listener
      */
     public void unRegisterMyTouchListener(MyTouchListener listener) {
-        myTouchListeners.remove( listener );
+        myTouchListeners.remove(listener);
     }
 
     /**
