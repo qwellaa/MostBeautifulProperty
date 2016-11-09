67c078157f18c8f3799919252890849017dc7f73
张作发 <291390214@qq.com>
2016-11-09 20:27:37 +0800

    完成简单聊天

--- app/src/main/java/com/lanou3g/mostbeautifulproperty/mine/uiview/chat/ECChatActivity.java
+++ app/src/main/java/com/lanou3g/mostbeautifulproperty/mine/uiview/chat/ECChatActivity.java
@@ -1,8 +1,48 @@
 package com.lanou3g.mostbeautifulproperty.mine.uiview.chat;
 
+import com.hyphenate.easeui.ui.EaseChatFragment;
+import com.lanou3g.mostbeautifulproperty.R;
+import com.lanou3g.mostbeautifulproperty.baseclass.BaseActivity;
+
 /**
  * Created by dllo on 16/11/9.
  */
 
-public class ECChatActivity {
+public class ECChatActivity extends BaseActivity {
+    private EaseChatFragment chatFragment;
+    @Override
+    protected int setLayout() {
+        return R.layout.ecchatactivity;
+    }
+
+    @Override
+    protected void initView() {
+        //自带的fragment
+        chatFragment = new EaseChatFragment();
+
+
+
+
+    }
+
+    @Override
+    protected void initData() {
+        chatFragment.setArguments(getIntent().getExtras());
+        getSupportFragmentManager().beginTransaction().add(R.id.ecchat_activity_framelayout, chatFragment).commit();
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+    }
 }
